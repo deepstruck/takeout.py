5f32932b0251175d95ad180d48be8cb6cb6ff042
deepstruck <46249196+deepstruck@users.noreply.github.com>
2019-08-01 22:50:20 -0700

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,7 @@
+# This piece of code is licensed under CC BY 4.0 by deepstruck.
+# Date: 2019-08-01
+# Description: A simple interactive story.
+# You can run this application on linux using 
+# $ ./takeout.py
+#	or
+# $ python takeout.py
---
+++ takeout.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+#
+#
+# This piece of code is licensed under CC BY 4.0 by deepstruck.
+# Date: 2019-08-01
+# Description: A simple interactive story.
+# You can run this application with python $ takeout.py or $ ./takeout.py
+
+import random
+import time
+
+def displayIntro():
+    print("It is towards the end of a long year.")
+    print("")
+    time.sleep(3)
+    print("You have a craving for some chinese food and decide to leave")
+    print("")
+    time.sleep(3)
+    print("Wow, it's really dark out")
+    time.sleep(2)
+    print ("")
+
+def choosePath():
+    path = ""
+    while path != "1" and path != "2":
+        path = input("Were you going to get the number one or the number two? (1 or 2): ")
+    
+    return path
+
+def checkPath(chosenPath):
+    print("You leave, contemplating how fresh the food will be on your plate")
+    time.sleep(3)
+    print ("")
+    print("Amazing...You can't see anything at all!")
+    time.sleep(3)
+    print("A lamp flickers on as you pass underneath")
+    print()
+    time.sleep(3)
+
+    correctPath = random.randint(1, 2)
+
+    if chosenPath == str(correctPath):
+        print("You have arrived at the Chinese restaurant!")
+        print("Mmmmmm! Tasty!")
+        time.sleep(3)
+    else:
+        print("It's dark again...")
+        time.sleep(3)
+        print ("")
+        time.sleep(2)
+        print("You are hit by a vehicle!")
+        time.sleep(1)
+        print ("You are dead!")
+        time.sleep(6)
+playAgain = "yes"
+while playAgain == "yes" or playAgain == "y":
+    displayIntro()
+    choice = choosePath()
+    checkPath(choice)
+    playAgain = input("Would you like to try again? (yes or y to continue playing): ")
